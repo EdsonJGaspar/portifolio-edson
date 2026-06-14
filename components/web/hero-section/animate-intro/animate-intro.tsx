3ac502944714fa6351e5c9667d0e2c64137aab4f
EdsonJGaspar <edsonjoaogaspar21@gmail.com>
2026-06-14 18:04:21 +0100

    upDate: actualizacao do copy

--- components/web/hero-section/animate-intro/animate-intro.tsx
+++ components/web/hero-section/animate-intro/animate-intro.tsx
@@ -5,7 +5,7 @@ import TypewriterComponent from "typewriter-effect";
 export function AnimateIntro() {
   return (
     <div className="text-lg sm:text-2xl h-32 px-8 text-center font-medium text-gray-400">
-      Meu nome é Gaspar, sou:
+      Meu nome é <strong>Edson João Gaspar</strong>, sou:
       <span className="text-gray-500 font-bold font-">
         <TypewriterComponent
           options={{
--- components/web/hero-section/hero-setcion.tsx
+++ components/web/hero-section/hero-setcion.tsx
@@ -28,7 +28,7 @@ export function HeroSection() {
       <div className="absolute z-10 flex flex-col items-center ">
         <ImageHero />
         <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl my-6 font-bold tracking-wide text-transparent bg-clip-text bg-linear-to-r from-blue-500 to-purple-600 pb-1.5">
-          Olá, sou um Engenheiro Informático
+          Engenheiro Informático
         </h1>
         <AnimateIntro />
         <LinkButtom
