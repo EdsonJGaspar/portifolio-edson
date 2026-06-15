68d66220a44b5de7d9b47d0c997547deff2f8a6e
EdsonJGaspar <edsonjoaogaspar21@gmail.com>
2026-06-15 23:39:15 +0100

    feat: adicionado a seccao skills

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { HeroSection } from "../components/web/hero-section/hero-setcion";
 import { Services } from "@/components/web/services/services";
 import { ResumeSection } from "@/components/web/resume-section/resume-section";
 import { ProjectSection } from "@/components/web/projects-section/projects-section";
+import { SkillSection } from "@/components/web/skill-section/skill-section";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Services />
       <ResumeSection />
       <ProjectSection />
+      <SkillSection />
     </div>
   );
 }
